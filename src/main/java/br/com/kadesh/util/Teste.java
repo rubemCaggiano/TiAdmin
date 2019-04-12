38fe6276f986e967c02fc5736ab25005af7fe7a4
rubemCaggiano <rubem.caggiano@gmail.com>
2019-04-12 17:50:30 -0300

    TProduto/Pedido

--- src/main/java/br/com/kadesh/util/Teste.java
+++ src/main/java/br/com/kadesh/util/Teste.java
@@ -4,16 +4,22 @@ package br.com.kadesh.util;
 import br.com.kadesh.dao.Dao;
 import br.com.kadesh.dao.GenericDAO;
 import br.com.kadesh.model.Cargo;
+import br.com.kadesh.model.Produto;
 
 
 public class Teste {
 
     public static void main(String[] args) {
-        Dao<Cargo> cargoDao = new GenericDAO<>(Cargo.class);
+//        Dao<Cargo> cargoDao = new GenericDAO<>(Cargo.class);
+        Dao<Produto> dao = new GenericDAO<>(Produto.class);
         
-        Cargo c = new Cargo(0, "Vendedor");
+        Produto p = new Produto(0, "Teste");
         
-        cargoDao.salvar(c);
+//        Cargo c = new Cargo(0, "Vendedor");
+        
+//        cargoDao.salvar(c);
+
+            dao.salvar(p);
     }
     
 }
